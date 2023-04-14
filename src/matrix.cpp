cea0b326a0800ff99b0185c57ff6e3ab70dc8214
kdevivanco <kdevivanco@gmail.com>
2023-04-14 17:24:52 -0500

    g

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -11,59 +11,66 @@ using namespace std;
 matrix::matrix(int rows, int cols){
     rows_ = rows;
     cols_ = cols;
-    values = new int*[rows_];
-    for (int i = 0; i < rows_; ++i) {
-        values[i] = new int[cols_];
-    }
+    int size = rows_ * cols_;
+    values = new int[size];
 }
+
+matrix::matrix(const matrix& other){
+    if(&other == this){
+        return;
+    }
+
+    rows_ = other.rows_;
+    cols_ = other.cols_;
+    values = new int[rows_*cols_];
+}
+
 
 matrix &matrix::operator=(const matrix& other){
     if (&other == this) {
         return *this;
     }
-    for (int i = 0; i < rows_; ++i) {
-        delete[] values[i];
-    }
-    delete[] values;
-
 
     rows_ = other.rows_;
     cols_ = other.cols_;
-
-    values = new int*[rows_];
-    for (int i = 0; i < rows_; ++i) {
-        values[i] = new int[cols_];
-    }
-
-
-    for (int i = 0; i < rows_; ++i) {
-        for (int j = 0; j < cols_; ++j) {
-            values[i][j] = other.values[i][j];
-        }
-    }
-
+    values = new int[rows_ * cols_];
+    for (size_t i = 0; i < other.rows_; ++i)
+        for (size_t j = 0; j < other.cols_; ++j)
+            values[i * cols_ + j] = other(i,j);
     return *this;
 }
 
-
-
 int main(){
+    matrix m(3, 3); //Esto funcona
+    m(1,1) = 1; //Esto funciona
+    cin >> m(1,2); //Esto funciona
+    cout<<m; //esto funciona
+    matrix mtestcop(3,3); //esto funciona
+    mtestcop = m; //esto funciona
+    cout<< "copytest\n"<<mtestcop;
+
+
+
     random_device rd;
-    utec::matrix m1(10, 20);
-    utec::matrix m2(30, 40);
+    utec::matrix m1(8, 9);
+    utec::matrix m2(4, 5);
     uniform_int_distribution<int> dis(0, 100);
-    for (size_t i = 0; i < m1.rows(); ++i) {
+    for (size_t i = 0; i < m1.rows(); ++i)
         for (size_t j = 0; j < m1.cols(); ++j)
             m1(i, j) = dis(rd);
-    }
+    // Copia
     utec::matrix m3 = m1;
-    for (size_t i = 0; i < m2.rows(); ++i) {
+    for (size_t i = 0; i < m2.rows(); ++i)
         for (size_t j = 0; j < m2.cols(); ++j)
             m2(i, j) = dis(rd);
-    }
     cout << m1.rows() << " " << m1.cols() << endl;
     cout << m2.rows() << " " << m2.cols() << endl;
-    swap(m1, m2);
+    cout << m1 << endl << m2 << endl;
+    // Move
+    std::swap(m1, m2); // El swap es lo que malogra las matrices, deja todos los valores en 0!
+    m2(0, 0) = 100;
     cout << m1.rows() << " " << m1.cols() << endl;
     cout << m2.rows() << " " << m2.cols() << endl;
+    cout << m1 << endl << m2 << endl << m3 <<endl;
+
 }
--- src/matrix.h
+++ src/matrix.h
@@ -12,20 +12,30 @@ namespace utec {
 
     class matrix {
     private:
-        int** values; // Puntero a puntero para representar la matriz dinámica
+        int* values; // Puntero a puntero para representar la matriz dinámica
         int rows_; // Número de filas de la matriz
         int cols_; // Número de columnas de la matriz
 
     public:
+        matrix() = default;
         // Constructor por parámetros
         matrix(int rows, int cols);
+
+        //Constructor copia
+        matrix(const matrix& other);
+
+        //Asignacion copia
+        matrix& operator=(const matrix& other);
+
+        //Constructor move
+
 
         // Destructor
         ~matrix() {
             // Liberar la memoria dinámica de la matriz
-            for (int i = 0; i < rows_; ++i) {
-                delete[] values[i];
-            }
+//            for (int i = 0; i < rows_; ++i) {
+//                delete[] values[i];
+//            }
             delete[] values;
         }
 
@@ -33,24 +43,39 @@ namespace utec {
             return this->rows_;
         }
 
-        // Sobrecarga de operador para acceder a los elementos de la matriz
-        int& operator()(int row, int col) {
-            return values[row][col];
+
+        // Sobrecarga de operador para asignar los elementos de la matriz
+        int& operator()(int row, int col) const {
+            return values[row * cols_ + col];
         }
 
-        matrix& operator=(const matrix& other);
+        //Sobre carga operador =
+
 
 
         // Sobrecarga de operador para imprimir la matriz
         friend ostream& operator<<(ostream& out, const matrix& m) {
             for (int i = 0; i < m.rows_; ++i) {
                 for (int j = 0; j < m.cols_; ++j) {
-                    out << setw(4) << m.values[i][j] << " "; // Espacio de 4 caracteres para cada elemento
+                    out << setw(4) <<  m(i, j) << " ";
+
                 }
-                out << endl;
+                std::cout << std::endl;
             }
             return out;
         }
+
+
+//        friend istream& operator>>(istream& in, matrix& m){
+//            for (size_t i = 0; i < m.rows_; ++i) {
+//                for (size_t j = 0; j < m.cols_; ++j) {
+//                    cout << m;
+//                    in >> m.values[i * m.cols_ + j];
+//                }
+//            }
+//            return in;
+//        }
+
 
         // Método para obtener el número de filas de la matriz
         int rows() const {
@@ -62,5 +87,6 @@ namespace utec {
             return cols_;
         }
     };
+    istream &operator>> (istream &in, matrix& m);
 }
 #endif //PROG3_UNIT1_TAREA_INTRODUCTORIA_V2022_1_MATRIX_H
