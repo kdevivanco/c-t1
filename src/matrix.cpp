b6f867113e2251bba28293c953052342d8048ecb
kdevivanco <kdevivanco@gmail.com>
2023-04-14 07:50:58 -0500

    primera construccion, falta ordenar archivos y agregar operadores

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "matrix.h"
+using namespace utec;
+using namespace std;
+
+Matrix::Matrix(int rows, int cols){
+    // Asignar memoria dinámica para la matriz
+    values = new int*[rows_];
+    for (int i = 0; i < rows_; ++i) {
+        values[i] = new int[cols_];
+    }
+}
+
+int main(){
+    
+}
--- src/matrix.h
+++ src/matrix.h
@@ -5,5 +5,59 @@
 #ifndef PROG3_UNIT1_TAREA_INTRODUCTORIA_V2022_1_MATRIX_H
 #define PROG3_UNIT1_TAREA_INTRODUCTORIA_V2022_1_MATRIX_H
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+namespace utec {
 
+    class Matrix {
+    private:
+        int** values; // Puntero a puntero para representar la matriz dinámica
+        int rows_; // Número de filas de la matriz
+        int cols_; // Número de columnas de la matriz
+
+    public:
+        // Constructor por parámetros
+        Matrix(int rows, int cols);
+
+        // Destructor
+        ~Matrix() {
+            // Liberar la memoria dinámica de la matriz
+            for (int i = 0; i < rows_; ++i) {
+                delete[] values[i];
+            }
+            delete[] values;
+        }
+
+        int getRows(){
+            return this->rows_;
+        }
+
+        // Sobrecarga de operador para acceder a los elementos de la matriz
+        int& operator()(int row, int col) {
+            return values[row][col];
+        }
+
+        // Sobrecarga de operador para imprimir la matriz
+        friend std::ostream& operator<<(std::ostream& os, const Matrix& m) {
+            for (int i = 0; i < m.rows_; ++i) {
+                for (int j = 0; j < m.cols_; ++j) {
+                    os << std::setw(4) << m.values[i][j] << " "; // Espacio de 4 caracteres para cada elemento
+                }
+                os << std::endl;
+            }
+            return os;
+        }
+
+        // Método para obtener el número de filas de la matriz
+        int rows() const {
+            return rows_;
+        }
+
+        // Método para obtener el número de columnas de la matriz
+        int cols() const {
+            return cols_;
+        }
+    };
+}
 #endif //PROG3_UNIT1_TAREA_INTRODUCTORIA_V2022_1_MATRIX_H
